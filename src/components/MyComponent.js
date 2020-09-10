43d688e29fc2f5853d0bdccdeeb42ccf2d7510cf
Rauf Ismayilov <rauf.raymond110@gmail.com>
2020-09-10 20:07:18 +0400

    yenilikler

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Second from "./Second"
+
+function MyComponent(){
+    return(
+        <div>
+            <h3>Our custom() component</h3>
+            <Second />
+        </div>
+    )
+}
+
+export default MyComponent;
---
+++ src/components/Second.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Second(){
+    return(
+        <div>
+            <h1>Second Component</h1>
+        </div>
+    )
+}
+
+export default Second;
